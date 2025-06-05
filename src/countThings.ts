c6b4fb4b590316ab6c2f1443b2eb6fac1aec272f
Piotr <hunter976@gmail.com>
2025-06-05 23:20:51 +0200

    fixed background patterns

--- src/countThings.ts
+++ src/countThings.ts
@@ -10,18 +10,18 @@ export const countBox: ResultBox[] = [
     id: 1,
     name: 'Total Characters',
     backgroundColor: '#d3a0fa',
-    backgroundPattern: `url('../../public/pattern-character-count.svg')`,
+    backgroundPattern: `url('pattern-character-count.svg')`,
   },
   {
     id: 2,
     name: 'Word Count',
     backgroundColor: '#ff9f00',
-    backgroundPattern: `url('../../public/pattern-word-count.svg')`,
+    backgroundPattern: `url('pattern-word-count.svg')`,
   },
   {
     id: 3,
     name: 'Sentence Count',
     backgroundColor: '#fe8159',
-    backgroundPattern: `url('../../public/pattern-sentence-count.svg')`,
+    backgroundPattern: `url('pattern-sentence-count.svg')`,
   },
 ];
